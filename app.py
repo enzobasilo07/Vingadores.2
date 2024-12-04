748c9ce3a6b2182e0845c96164fe3f93d3b3594d
Enzo Basilio Rickamnn <159261125+enzobasilo07@users.noreply.github.com>
2024-12-04 14:24:52 -0300

    Add files via upload

---
+++ app.py
@@ -0,0 +1,11 @@
+from registro.herois import Heroi
+from registro.interface import Interface as i
+from model.databse import Databases
+
+def main():
+    
+
+    i.apresentar_menu_principal()
+
+if __name__ =='__main__':
+    main()
